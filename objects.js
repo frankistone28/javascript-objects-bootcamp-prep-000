b031ee5a46692527a800bc567844d71541cb223e
Franklyn Thompson <frankistone28@outlook.com>
2017-09-23 08:28:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {"kevin gates" : "Out the mud"};
 function updatePlaylist(playlist,artistName,songTitle){
     return Object.assign{{},playlist,{[artistName] : songTitle}}
 }
+
+function removeFromPlaylist(playlist,artistName){
+    return delete playlist[artistName];
+}
